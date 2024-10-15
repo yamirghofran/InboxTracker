6b6cb6650b784ac55176a48a2441071f759d3988
Yousef Amirghofran <yamirghofran@gmail.com>
2024-10-15 14:19:13 +0200

    General Commit

--- frontend/app/sessions.ts
+++ frontend/app/sessions.ts
@@ -23,7 +23,7 @@ const { getSession, commitSession, destroySession } =
         //
         // expires: new Date(Date.now() + 60_000),
         httpOnly: true,
-        maxAge: 60,
+        maxAge: 60 * 60 * 24 * 30, // 30 days
         //path: "/",
         //sameSite: "lax",
         secrets: ["s3cret1"],
