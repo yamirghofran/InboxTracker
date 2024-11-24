fc23aa61653ecd1d7ece75bd8cb8227f9bdb5b9d
javidominguezsegura <javier.dominguez.segura@gmail.com>
2024-11-24 15:12:57 +0100

    not finished

---
+++ backend/azure/resources.bash
@@ -0,0 +1,54 @@
+# !/bin/bash
+
+
+# 1) Setting up locally
+
+mkdir backend
+cd backend
+
+export RESOURCE_GROUP="CC_A_7"
+export STORAGE_ACCOUNT_NAME="theboyz0"
+export FUNCTION_APP_NAME="InboxTracker"
+
+func AzureFunctions init --python -m V2
+
+cd AzureFunctions
+
+func new --template "Http Trigger" --name "CreateExpense" --auth-level "Function" # We created a single function and added all others inside its module; Done to concentrate all serverless funcs code in a single place
+
+pip3 install --no-cache-dir -r requirements.txt
+
+# 2) Logging in Azure
+
+az login --use-device-code
+
+az account set
+
+az account list --output table # Check if the account is set correctly
+
+# Creating storage account
+
+az storage account create --name $STORAGE_ACCOUNT_NAME --resource-group $RESOURCE_GROUP  --sku "Standard_LRS"
+
+
+# Creating function app
+
+az functionapp create --consumption-plan-location "westeurope" \
+      --resource-group $RESOURCE_GROUP \
+      --runtime "python:3.11" \
+      --functions-version 4 \
+      --name $FUNCTION_APP_NAME \
+      --storage-account $STORAGE_ACCOUNT_NAME \
+      --os-type "linux"ç
+
+# 3) Testing locally
+func azure functionapp fetch-app-settings $FUNCTION_APP_NAME
+port=$RANDOM
+func start --port $port
+
+curl http://localhost:$port/api/CreateExpense
+
+
+# 4) Deploying the function app
+
+func azure functionapp publish $FUNCTION_APP_NAME
